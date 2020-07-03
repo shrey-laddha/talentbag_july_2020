8258dac89b533467457e0e25346ef555c817c03d
shreytiger <55894727+shreytiger@users.noreply.github.com>
2020-07-03 22:01:55 +0530

    Add files via upload

---
+++ Task2/script.js
@@ -0,0 +1,25 @@
+         function dragStart(ev) {
+            ev.dataTransfer.effectAllowed='move';
+            ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+            ev.dataTransfer.setDragImage(ev.target,0,0);
+            return true;
+         }
+         function dragEnter(ev) {
+            event.preventDefault();
+            return true;
+         }
+         function dragOver(ev) {
+            return false;
+         }
+         function dragDrop(ev) {
+            var src = ev.dataTransfer.getData("Text");
+            ev.target.appendChild(document.getElementById(src).cloneNode(true));
+            ev.stopPropagation();
+            return false;
+            dataTransfer.addElement(src);
+
+         }
+            // // Store
+            // localStorage.setItem(src);
+            // // Retrieve
+            // document.getElementById(src).innerHTML = localStorage.getItem(src);
